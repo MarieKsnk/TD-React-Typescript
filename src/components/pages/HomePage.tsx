9fa18f0f0d4ef71026d5c3662df89ce0c77fe10c
Marie Klisnick <marie63.klisnick@gmail.com>
2025-02-01 18:57:15 +0100

    Ajout des favoris sur la homepage

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -2,14 +2,33 @@ import { Header } from "@organisms/Header";
 import { Button } from "@atoms/Button";
 import { Link } from "react-router-dom";
 import "./globals.css"
+import { useFavoriteStore } from "@store/useFavoriteStore";
+import { RecipeCard } from "@molecules/RecipeCard";
 
 export function HomePage() {
+
+  const favorites  = useFavoriteStore((state) => state.favorites);
 
     return (
     <>
      <Header />
       <main>
         <h1 className="title">Bienvenu</h1>
+        <h2> Mes recettes favorites :</h2>
+          {favorites.length === 0 ? (
+            <p>Vous n'avez pas encore de favoris...</p>
+          ) : (
+            <div className="favorites-list">
+              {favorites.map((recipe) => (
+                <RecipeCard
+                  key={recipe.idMeal}
+                  id={recipe.idMeal}
+                  title={recipe.strMeal}
+                  image={recipe.strMealThumb}
+                />
+              ))}
+            </div>
+          )}
         <Link to="/recettes">
           <Button label="Voir toutes les recettes" />
         </Link>
